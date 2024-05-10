b32a74b67162830c9db1870107bcd32aee79f19d
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-05-10 21:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function isSameType(value1, value2) {
 
 	if (Number.isNaN(value1) && Number.isNaN(value2)) {
 		return true;
+	}
 		let check = typeof value1;
-	let check2 = typeof value2;
-	if (check===check2) {
+		let check2 = typeof value2;
+		if (check===check2) {
 		return true;
-	}
-	}
+		}
 	
 	else{
 		return false;
